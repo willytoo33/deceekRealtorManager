b2718e9328280b76c29d4429a12b54088b7f60ce
Kiptoo Willy <willytoo33@gmail.com>
2024-06-28 16:16:27 +0300

    Header Finished

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  menuValue: boolean = false;
+  menu_icon: string = 'bi bi-list';
 
+  toggleMenu() {
+    this.menuValue = !this.menuValue
+    this.menu_icon = this.menuValue ? 'bi bi-x' : 'bi bi-list';
+  }
+
+  closeMenu() {
+    this.menuValue = false;
+    this.menu_icon = 'bi bi-list';
+  }
 }
