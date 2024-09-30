fc4140125be59b0efcdd74d8b61a3c9bbfcdedf2
Kiptoo Willy <willytoo33@gmail.com>
2024-09-30 08:34:07 +0300

    contact section completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomepageComponent],
+  imports: [RouterOutlet, HeaderComponent, HomepageComponent, ContactFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ContactFormComponent } from "../contact-form/contact-form.component";
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [ContactFormComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
