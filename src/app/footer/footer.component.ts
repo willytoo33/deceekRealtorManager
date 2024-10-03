f3fb657e02dffd3aa71ce496afffe9743d5cf529
Kiptoo Willy <willytoo33@gmail.com>
2024-10-03 10:24:08 +0300

    done till the footer, save for responsiveness

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
 
 @Component({
   selector: 'app-footer',
@@ -13,6 +14,10 @@ export class FooterComponent {
   year: number = new Date().getFullYear();
   company: string = 'Deceek Property Management'
 
-  faCoffee = faCoffee
+  faCoffee = faCoffee;
+  faLinkedin = faLinkedin
+  faInstagram = faInstagram
+  faWhatsapp = faWhatsapp
+  
 
 }
