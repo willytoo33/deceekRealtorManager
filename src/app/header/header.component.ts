c61ecc198dc6fc728bda9e349150f78d36b8b7cf
Kiptoo Willy <willytoo33@gmail.com>
2024-12-25 13:59:22 +0300

    Fixed header issues

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,14 +5,14 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   menuValue: boolean = false;
   menu_icon: string = 'bi bi-list';
 
   toggleMenu() {
-    this.menuValue = !this.menuValue
+    this.menuValue = !this.menuValue;
     this.menu_icon = this.menuValue ? 'bi bi-x' : 'bi bi-list';
   }
 
